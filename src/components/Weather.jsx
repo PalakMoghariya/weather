5c0de24f6540c264a7526739ee20d2e4e42f26cd
PalakMoghariya <palakmoghariya4@gmail.com>
2024-11-02 19:41:43 +0530

    Add Navbar

---
+++ src/components/Weather.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Weather() {
+  return (
+    <div className='img-fluid'>
+            {/* <h1 className='h1-weather'>OpenWeather</h1> */}
+    </div>
+  )
+}
